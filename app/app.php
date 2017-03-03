00c78cab11d8a5ae88d82cb30c1d642b957ab162
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-03-03 10:32:08 -0800

    add paths to index.php, app.php and display Hello World

---
+++ app/app.php
@@ -0,0 +1,27 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    $app = new Silex\Application();
+    $app->register(
+       new Silex\Provider\TwigServiceProvider(),
+       array('twig.path' => __DIR__.'/../views')
+    );
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+    $app['debug'] = true;
+
+
+    //INDEX PAGE GET
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
