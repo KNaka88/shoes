237f28deb3313fbf99e294839944e34eaa7f5892
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-03-03 11:30:44 -0800

    add brand specific page

--- app/app.php
+++ app/app.php
@@ -21,26 +21,44 @@
 
     //INDEX PAGE GET
     $app->get("/", function() use ($app){
-        return $app['twig']->render('index.html.twig', array("stores" => Store::getAll()));
+        return $app['twig']->render('index.html.twig', array("stores" => Store::getAll(), "brands" => Brand::getAll()));
     });
+    $app->post("/", function() use ($app){
 
-    //Create STORE
-    $app->post("/create_store", function() use ($app){
-        $new_store = new Store($_POST['store_name']);
-        $new_store->save();
+        $input_name = filter_var($_POST['name'], FILTER_SANITIZE_MAGIC_QUOTES);
+        $input_type = $_POST['type'];
+
+        if($input_type == "store"){
+          $new_store = new Store($input_name);
+          $new_store->save();
+
+        }elseif($input_type == "brand"){
+          $new_brand = new Brand($input_name);
+          $new_brand->save();
+                  }
         return $app->redirect('/');
     });
 
-
     //Store PAGE
+    $app->post("/store/{id}", function($id) use ($app){
+      $store = Store::find($id);
+      return $app->redirect("/store/".$store.getId());
+    });
     $app->get("/store/{id}", function($id) use ($app){
         $store = Store::find($id);
         return $app['twig']->render("store.html.twig", array("store"=>$store));
     });
 
-    $app->post("/store/{id}", function($id) use ($app){
-        $store = Store::find($id);
-        return $app->redirect("/store/".$store.getId());
+
+
+    //Brand PAGE
+    $app->post("/brand/{id}", function($id) use ($app){
+      $brand = Brand::find($id);
+      return $app->redirect("/brand/".$brand.getId());
+    });
+    $app->get("/brand/{id}", function($id) use ($app){
+        $brand = Brand::find($id);
+        return $app['twig']->render("brand.html.twig", array("brand"=>$brand));
     });
 
 
